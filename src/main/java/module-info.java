3173aa1435c485ccabeff3b2f48778fea2a1e2cf
kindarnakes <kindenene@gmail.com>
2020-10-29 12:08:07 +0100

    Init

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module org.ciclo {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+    requires javafx.graphics;
+
+    //opens org.ciclo.model to mysql.connector.java;
+    //exports org.ciclo;
+    opens org.ciclo to javafx.fxml;
+    exports org.ciclo to javafx.graphics;
+
+    opens org.ciclo.controller to javafx.fxml;
+    exports org.ciclo.controller;
+}
