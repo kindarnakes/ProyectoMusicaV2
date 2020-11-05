56f5c54ae0182109b4638bc88178a51da3ce2148
kindarnakes <kindenene@gmail.com>
2020-11-05 18:14:42 +0100

    Minor additions

--- src/main/java/org/ciclo/model/SongDAO.java
+++ src/main/java/org/ciclo/model/SongDAO.java
@@ -17,7 +17,7 @@ public class SongDAO extends Song {
         super();
     }
 
-    public SongDAO(Song s){
+    public SongDAO(Song s) {
         this.setId(s.getId());
         this.setDuration(s.getDuration());
         this.setName(s.getName());
@@ -25,6 +25,10 @@ public class SongDAO extends Song {
         this.setDisc(s.getDisc()); //Pointer to same object
         this.setList(s.getLists()); //Pointer to same object
         this.setReproductions(null);
+    }
+
+    public SongDAO(Integer id){
+        this(SongDAO.listById(id));
     }
 
     public static List<Song> listAll() {
@@ -35,11 +39,21 @@ public class SongDAO extends Song {
                 PreparedStatement st = conn.prepareStatement(sql);
                 ResultSet rs = st.executeQuery()
         ) {
+            List<Disc> discs = DiscDAO.listAll();
             while (rs != null && rs.next()) {
                 Song aux = new Song();
                 aux.setId(rs.getInt("id"));
                 aux.setName(rs.getString("nombre"));
                 aux.setDuration(rs.getInt("duracion"));
+                boolean find = false;
+                int index = 0;
+                for (int i = 0; i < discs.size() && !find; i++) {
+                    if (discs.get(i).getId() == rs.getInt("id_disco")) {
+                        find = true;
+                        index = i;
+                    }
+                }
+                aux.setDisc(discs.get(index));
                 songs.add(aux);
             }
         } catch (SQLException ex) {
@@ -48,27 +62,28 @@ public class SongDAO extends Song {
         return songs;
     }
 
-    public static List<Song> listById(Integer id) {
-        List<Song> songs = new ArrayList<>();
+    public static Song listById(Integer id) {
+        Song song = new Song();
         String sql = "SELECT id, nombre, duracion FROM cancion WHERE id = ?";
         try (
                 Connection conn = org.ciclo.model.connect.Connection.getConnect();
-                PreparedStatement st = conn.prepareStatement(sql);
+                PreparedStatement st = conn.prepareStatement(sql)
         ) {
             st.setInt(1, id);
             ResultSet rs = st.executeQuery();
-            while (rs != null && rs.next()) {
-                Song aux = new Song();
-                aux.setId(rs.getInt("id"));
-                aux.setName(rs.getString("nombre"));
-                aux.setDuration(rs.getInt("duracion"));
-                songs.add(aux);
-            }
+            Disc disc = DiscDAO.listById(rs.getInt("id_disco")).get(0);//MUST return one
+            Song aux = new Song();
+            aux.setId(rs.getInt("id"));
+            aux.setName(rs.getString("nombre"));
+            aux.setDuration(rs.getInt("duracion"));
+            aux.setDisc(disc);
+            song = aux;
+
             rs.close();
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
-        return songs;
+        return song;
     }
 
     public static List<Song> listByName(String name) {
@@ -76,16 +91,17 @@ public class SongDAO extends Song {
         String sql = "SELECT id, nombre, duracion, id_disco FROM cancion WHERE nombre = ?";
         try (
                 Connection conn = org.ciclo.model.connect.Connection.getConnect();
-                PreparedStatement st = conn.prepareStatement(sql);
+                PreparedStatement st = conn.prepareStatement(sql)
         ) {
             st.setString(1, name);
             ResultSet rs = st.executeQuery();
             while (rs != null && rs.next()) {
+                Disc disc = DiscDAO.listById(rs.getInt("id_disco")).get(0);//MUST return one|Name not UNIQUE but not have many repetitions
                 Song aux = new Song();
                 aux.setId(rs.getInt("id"));
                 aux.setName(rs.getString("nombre"));
                 aux.setDuration(rs.getInt("duracion"));
-                //añadir disco
+                aux.setDisc(disc);
                 songs.add(aux);
             }
             rs.close();
@@ -95,19 +111,19 @@ public class SongDAO extends Song {
         return songs;
     }
 
-    public boolean update(){
-        boolean update =false;
+    public boolean update() {
+        boolean update = false;
         String sql = "UPDATE cancion SET nombre = ?, duration = ?, id_disco = ? FROM cancion WHERE id = ?";
         try (
                 Connection conn = org.ciclo.model.connect.Connection.getConnect();
-                PreparedStatement st = conn.prepareStatement(sql);
+                PreparedStatement st = conn.prepareStatement(sql)
         ) {
             st.setString(1, this.getName());
             st.setInt(2, this.getDuration());
             st.setInt(3, this.getDisc().getId());
             st.setInt(4, this.getId());
             int i = st.executeUpdate();
-            if(i>1){
+            if (i > 1) {
                 update = true;
             }
         } catch (SQLException ex) {
@@ -118,19 +134,19 @@ public class SongDAO extends Song {
         return update;
     }
 
-    public boolean save(){
+    public boolean save() {
         boolean saved = false;
 
         String sql = "INSERT INTO cancion(nombre, duration, id_disco) VALUES (?, ?, ?)";
         try (
-                Connection conn = org.ciclo.model.connect.Connection.getConnect();
+                Connection conn = org.ciclo.model.connect.Connection.getConnect()
         ) {
             PreparedStatement st = conn.prepareStatement(sql);
             st.setString(1, this.getName());
             st.setInt(2, this.getDuration());
             st.setInt(3, this.getDisc().getId());
             int i = st.executeUpdate();
-            if(i>1){
+            if (i > 1) {
                 saved = true;
             }
 
@@ -138,7 +154,7 @@ public class SongDAO extends Song {
             st = conn.prepareStatement(sql);
             st.setString(1, this.getName());
             ResultSet rs = st.executeQuery();
-            if(rs !=null && rs.next()){
+            if (rs != null && rs.next()) {
                 this.setId(rs.getInt("id"));
             }
             rs.close();
@@ -150,16 +166,16 @@ public class SongDAO extends Song {
         return saved;
     }
 
-    public static  boolean remove(Integer id){
-        boolean removed =false;
+    public static boolean remove(Integer id) {
+        boolean removed = false;
         String sql = "DELETE FROM canciones WHERE id = ?";
         try (
                 Connection conn = org.ciclo.model.connect.Connection.getConnect();
-                PreparedStatement st = conn.prepareStatement(sql);
+                PreparedStatement st = conn.prepareStatement(sql)
         ) {
             st.setInt(1, id);
             int i = st.executeUpdate();
-            if(i>1){
+            if (i > 1) {
                 removed = true;
             }
         } catch (SQLException ex) {
@@ -168,16 +184,16 @@ public class SongDAO extends Song {
         return removed;
     }
 
-    public  boolean remove(){
-        boolean removed =false;
+    public boolean remove() {
+        boolean removed = false;
         String sql = "DELETE FROM canciones WHERE id = ?";
         try (
                 Connection conn = org.ciclo.model.connect.Connection.getConnect();
-                PreparedStatement st = conn.prepareStatement(sql);
+                PreparedStatement st = conn.prepareStatement(sql)
         ) {
             st.setInt(1, this.getId());
             int i = st.executeUpdate();
-            if(i>1){
+            if (i > 1) {
                 removed = true;
             }
         } catch (SQLException ex) {
