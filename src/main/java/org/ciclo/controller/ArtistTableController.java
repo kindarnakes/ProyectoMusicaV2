f9e118256fbb85ce1a2f8a9da57b39b169a0a626
kindarnakes <kindenene@gmail.com>
2020-11-12 12:53:09 +0100

    GUI added methods

--- src/main/java/org/ciclo/controller/ArtistTableController.java
+++ src/main/java/org/ciclo/controller/ArtistTableController.java
@@ -3,6 +3,7 @@ package org.ciclo.controller;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -11,6 +12,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import org.ciclo.MainApp;
@@ -18,8 +20,11 @@ import org.ciclo.MainApp;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Predicate;
+
 import org.ciclo.model.Artist;
 import org.ciclo.model.ArtistDAO;
+import org.ciclo.model.Disc;
 
 public class ArtistTableController implements Initializable {
 
@@ -31,8 +36,12 @@ public class ArtistTableController implements Initializable {
     TableColumn<Artist, String> c2;
     @FXML
     TableColumn<Artist, String> c3;
+    @FXML
+    TextField filter;
 
     private ObservableList<Artist> _list;
+    private FilteredList<Artist> _listArtistFiltered;
+
 
 
     @Override
@@ -43,11 +52,23 @@ public class ArtistTableController implements Initializable {
                     update();
                 }
             });
+
+        filter.textProperty().addListener((observable, oldValue, newValue) -> {
+            filter();
+        });
+
+    }
+
+    private void filter() {
+
+        Predicate<Artist> songPredicate = i -> i.getName().startsWith(filter.getText());
+        if( _listArtistFiltered != null){_listArtistFiltered.setPredicate(songPredicate);}
     }
 
     public void updateTable() {
         _list = FXCollections.observableList(ArtistDAO.List_All_Artist());
-        tableExample.setItems(_list);
+        _listArtistFiltered = new FilteredList<>(_list);
+        tableExample.setItems(_listArtistFiltered);
         c1.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getName()));
         c2.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getFrom()));
         c3.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getPhoto()));
