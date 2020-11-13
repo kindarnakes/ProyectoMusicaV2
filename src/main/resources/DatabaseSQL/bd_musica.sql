7367f47070b3c697ebd819d9de20a4c7e79306d1
kindarnakes <kindenene@gmail.com>
2020-11-13 12:50:28 +0100

    Update bd_musica.sql
    bug correction

--- src/main/resources/DatabaseSQL/bd_musica.sql
+++ src/main/resources/DatabaseSQL/bd_musica.sql
@@ -21,7 +21,7 @@ SET time_zone = "+00:00";
 -- Base de datos: `bd_musica`
 --
 CREATE DATABASE IF NOT EXISTS db_musica;
-USE bd_musica;
+USE db_musica;
 -- --------------------------------------------------------
 
 --
