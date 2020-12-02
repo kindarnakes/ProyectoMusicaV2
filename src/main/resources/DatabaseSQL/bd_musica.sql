5128aefcf15fbc03b5333532d570d15546618f02
kindarnakes <kindenene@gmail.com>
2020-12-02 21:01:17 +0100

    Update bd_musica.sql

--- src/main/resources/DatabaseSQL/bd_musica.sql
+++ src/main/resources/DatabaseSQL/bd_musica.sql
@@ -20,7 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `bd_musica`
 --
-
+CREATE DATABASE IF NOT EXISTS bd_musica;
+USE bd_musica;
 -- --------------------------------------------------------
 
 --
