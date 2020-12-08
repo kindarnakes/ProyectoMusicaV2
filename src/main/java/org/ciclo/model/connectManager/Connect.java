d5e0d9258ceb8dddcfc80a8796286b7016f2eff5
Kin <kindenene@gmail.com>
2020-12-08 19:39:47 +0100

    Try-catch

--- src/main/java/org/ciclo/model/connectManager/Connect.java
+++ src/main/java/org/ciclo/model/connectManager/Connect.java
@@ -7,12 +7,17 @@ import javax.persistence.Persistence;
 
 public class Connect {
 
-    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("MariaDB");
+    private static final String PERSISTENCE_UNIT = "MariaDB";
+    private static EntityManagerFactory emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT);
     private static EntityManager manager = null;
 
     public static EntityManager getManager() {
-        if (manager == null || !manager.isOpen()) {
-            manager = emf.createEntityManager();
+        try {
+            if (manager == null || !manager.isOpen()) {
+                manager = emf.createEntityManager();
+            }
+        }catch (IllegalStateException ex){
+            emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT);
         }
         return manager;
     }
