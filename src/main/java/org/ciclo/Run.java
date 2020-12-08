91765369f3f97512cb642df87b64448aeedc9fe6
Kin <kindenene@gmail.com>
2020-12-08 18:09:33 +0100

    Final?

--- src/main/java/org/ciclo/Run.java
+++ src/main/java/org/ciclo/Run.java
@@ -1,16 +1,9 @@
 package org.ciclo;
 
-import org.ciclo.model.*;
-
-import java.util.TreeSet;
-
 public class Run {
 
     public static void main(String[] args) {
         MainApp.main(args);
-
-
-
 
     }
 }
